f78fb963d832fd7d6c5c9a7d18b7714483901534
David Kosslyn <david@kosslyn.com>
2010-12-03 11:17:11 -0800

    Print out gallery name when it loads.

--- client/includes/WAG.js
+++ client/includes/WAG.js
@@ -1,3 +1,4 @@
+// WAG = "Wikipedia Art Gallery" :)
 function WAG(canvas) {
   this.canvas = document.getElementById(canvas);
 
@@ -79,6 +80,11 @@ function WAG(canvas) {
     if (data.resultCode != 0) {
       alert("Error in retrieving category: " + data.resultCode);
     } else {
+      $('#loadingtext').fadeIn("fast");
+      // substring below removes the "category:" at the start of the str
+      $('#loadingtext').text("Gallery of " + data.category.substring(9));
+      $('#loadingtext').fadeOut(5000);
+    
       // Add the new room to the map
       var oldContinueKey = "";
       if (data.oldContinueKey) {
@@ -98,7 +104,6 @@ function WAG(canvas) {
         this.map[this.prevPointer.key][this.prevPointer.dir] = this.currentRoom;
       }
 
-      $('#loadingtext').hide();
       room.clearPaintings();
       for (var i in data.data) {
         var preload = new Image();
